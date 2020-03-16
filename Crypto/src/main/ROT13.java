a401d6050e432a98cb24c634fdc3f8999f916f4d
Sandy Setiawan <ssetiawan@zipcodes-MBP-3.fios-router.home>
2020-03-15 21:06:12 -0400

    completed

--- Crypto/src/main/ROT13.java
+++ Crypto/src/main/ROT13.java
@@ -6,7 +6,7 @@ public class ROT13 {
 
 
     ROT13(Character cs, Character cf) {
-        this.shift= (cf - cs);
+        this.shift = (cf - cs);
 
     }
 
@@ -21,17 +21,33 @@ public class ROT13 {
     }
 
         public String encrypt ( String text){
-            return text;
+            String result = rotateByCharacterPosition(text, shift);
+            return result;
         }
 
-        public String decrypt ( String text){
-            return text;
+
+        public String decrypt (String text){
+            String result = rotateByCharacterPosition(text, shift);
+            return result;
         }
 
         public static String rotate ( String s, Character c){
 
-            return "";
+            int characterIndex = s.indexOf(c);
+            StringBuilder sb = new StringBuilder();
+            sb.append(s.substring(characterIndex) + s.substring(0, characterIndex));
+            return sb.toString();
         }
 
+    public String rotateByCharacterPosition(String s, int shift) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isAlphabetic(s.charAt(i)) && Character.isLowerCase(s.charAt(i))) {
+                sb.append((char) (((s.charAt(i) - 'a' + shift) % 26) + 'a'));
+            } else if (Character.isAlphabetic(s.charAt(i)) && Character.isUpperCase(s.charAt(i))) {
+                sb.append((char) (((s.charAt(i) - 'A' + shift) % 26) + 'A'));
+            } else sb.append(s.charAt(i));
+        }
+        return sb.toString();
     }
 }
